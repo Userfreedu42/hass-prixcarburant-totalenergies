733a11a936e7efe11b33e1781513ae8a4c9ec7a0
Userfreedu42 <emailpourlespubs@laposte.net>
2026-09-25 22:49:19 +0200

    Delete hass-prixcarburant-totalenergies-v2/hass-prixcarburant-totalenergies-v2 directory

--- hass-prixcarburant-totalenergies-v2/hass-prixcarburant-totalenergies-v2/custom_components/prix_carburant_totalenergies/const.py
+++
@@ -1,5 +0,0 @@
-DOMAIN="prix_carburant_totalenergies"
-DEFAULT_SCAN_MINUTES=10
-DEFAULT_RADIUS_KM=15
-INSTANT_URL="https://donnees.roulez-eco.fr/opendata/instantane_ruptures"
-FUEL_LABELS={"gazole":"Gazole","sp95":"SP95","sp98":"SP98","e10":"E10","e85":"E85","gplc":"GPLc"}
