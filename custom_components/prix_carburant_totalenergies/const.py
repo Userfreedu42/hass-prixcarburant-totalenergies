6bcf75fed116e0bfde22f4d0b36e7400532d1342
Userfreedu42 <emailpourlespubs@laposte.net>
2026-09-25 23:24:19 +0200

    Fix broken station list URL

--- custom_components/prix_carburant_totalenergies/const.py
+++ custom_components/prix_carburant_totalenergies/const.py
@@ -2,5 +2,4 @@ DOMAIN = "prix_carburant_totalenergies"
 DEFAULT_SCAN_MINUTES = 10
 DEFAULT_RADIUS_KM = 15
 INSTANT_URL = "https://donnees.roulez-eco.fr/opendata/instantane_ruptures"
-STATIONS_URL = "https://raw.githubusercontent.com/Userfreedu42/hass-prixcarburant-totalenergies/main/hass-prixcarburant-totalenergies-v2/hass-prixcarburant-totalenergies-v2/custom_components/prix_carburant_totalenergies/stations.json"
 FUEL_LABELS = {"gazole": "Gazole", "sp95": "SP95", "sp98": "SP98", "e10": "E10", "e85": "E85", "gplc": "GPLc"}
