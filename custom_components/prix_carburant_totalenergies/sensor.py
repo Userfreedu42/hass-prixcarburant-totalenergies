a12d861d10f6e88027591b40321c8ae0db1c85a3
Userfreedu42 <emailpourlespubs@laposte.net>
2026-09-26 18:53:30 +0200

    fix: use correct Home Assistant DeviceInfo import

--- custom_components/prix_carburant_totalenergies/sensor.py
+++ custom_components/prix_carburant_totalenergies/sensor.py
@@ -1,6 +1,6 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.const import UnitOfVolume
-from homeassistant.helpers.device_registry import DeviceInfo
+from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import DOMAIN, FUEL_LABELS
